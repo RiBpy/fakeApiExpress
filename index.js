44ca37e7da9c093c2fdf90ca5f788c734f690fec
ribpy <mdriazbappy22@gmail.com>
2023-01-10 16:22:07 +0600

    fake api backend

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+require("dotenv").config()
+const port = process.env.PORT||7000
+const {readdirSync}=require("fs")
+//console.log(readdirSync("./routes")); to get all routes from routes folder
+readdirSync("./routes").map(file=>app.use("/",require("./routes/"+file)))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.listen(port, () => {
+  console.log(`Fake API store running on port ${port}`)
+})
